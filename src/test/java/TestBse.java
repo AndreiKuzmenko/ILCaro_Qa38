44960e34d0c7aba26a669e1090918e539eb80b37
AndreiKuzmenko <eccehomo28@gmail.com>
2023-07-03 15:45:36 +0300

    03072023-study

--- src/test/java/TestBse.java
+++ src/test/java/TestBse.java
@@ -1,8 +1,12 @@
 import manager.Application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
 
 public class TestBse {
     Logger logger = LoggerFactory.getLogger(TestBse.class);
@@ -16,5 +20,16 @@ public class TestBse {
     @AfterSuite
     public void stop(){
         app.tearDown();
+    }
+
+    @BeforeMethod
+    public void startLogger (Method method) {
+        logger.info("Method " + method.getName() + " is started");
+
+    }
+    @AfterMethod
+    public  void end(){
+        logger.info("================================================================");
+
     }
 }

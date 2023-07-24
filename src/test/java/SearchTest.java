ac3bcb37c4d145f63eb8e8f37e9e8c17725e8b3c
AndreiKuzmenko <eccehomo28@gmail.com>
2023-07-24 19:28:39 +0300

    23072023-study

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -4,7 +4,7 @@ public class SearchTest extends TestBse{
 
     @Test
     public void searchPositiveTest(){
-        app.getSearch().fillSearchForm("Haifa", "07/15/2023", "07/19/2023");
+        app.getSearch().fillSearchForm("Haifa", "07/20/2023", "09/19/2023");
         app.getSearch().pause(1000);
         app.getSearch().submitForm();
     }
